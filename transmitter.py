bfd267674a697b92f4d9037ecbbefc46164f44b7
Alex <taffeAlexander@gmail.com>
2022-04-03 22:24:25 -0700

    Added interval as an arg
    unfinished

--- transmitter.py
+++ transmitter.py
@@ -47,14 +47,23 @@ def start_camera():
 
 
 def print_help_message():
-	print("Usage: transmitter.py --host <hostname> --port <port>")
+	print("""Usage: python transmitter.py [OPTIONS]
+	Takes images at a specified interval and sends them to a receiver.
+	
+	Available options:
+		--host				The hostname of the receiver e.g. 11.0.0.6 (default is localhost)
+		--port				The port of the receiver e.g. 5000 (default is 5000)
+	-i, --interval		The amount of time between images in minutes. (default is 10 minutes)
+	-h					Dispalys this help menu.
+	""")
 
 
 if __name__ == "__main__":
 	receiver_host = ''
 	receiver_port = ''
+	interval = ''
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "h", ["host=", "port="])
+		opts, args = getopt.getopt(sys.argv[1:], "hi:", ["host=", "port=", "interval="])
 	except getopt.GetoptError:
 		print_help_message()
 		sys.exit(2)
@@ -66,17 +75,22 @@ if __name__ == "__main__":
 			receiver_host = arg
 		elif opt in "--port":
 			receiver_port = arg
+		elif opt in ["-i", "--interval"]:
+			interval = arg
 
 	if not receiver_host:
-		print("Missing receiver host.")
-		print_help_message()
-		sys.exit()
+		print("Missing receiver host, defaulting to localhost.")
+		receiver_host = "localhost"
 
 	if not receiver_port:
 		print("Missing receiver port, defaulting to 5000.")
 		receiver_port = "5000"
 
-	print("Starting time lapse transmitter with host: {} and port: {}".format(receiver_host, receiver_port))
+	if not interval:
+		print("Missing interval defaulting to every 10 minutes")
+		interval = 10
+
+	print("Starting time lapse transmitter with host: {}, port: {}, and interval: every {} minutes.".format(receiver_host, receiver_port, interval))
 	camera = start_camera()
 	send_image(camera, receiver_host, receiver_port)
 	schedule.every(10).seconds.do(send_image, cam=camera, host=receiver_host, port=receiver_port)
